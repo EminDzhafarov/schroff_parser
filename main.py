f07cbd1751b0b53eb4dcfa306c42491fbf439048
Emin <emin.djaf@gmail.com>
2023-08-14 19:23:09 +0300

    Экспорт в XML

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 import os
 import time
+from datetime import datetime
+
 import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from lxml import etree
 
 
 download_dir = ''  # Директория для документов
@@ -23,7 +26,7 @@ driver = webdriver.Chrome(options=chrome_options)
 
 items = []
 
-for page in range(0, 1000, 10):
+for page in range(0, 10, 10):
     url = f"https://schroff.nvent.com/ru-ru/search#first={page}&t=Tab_All&f:@nventtype=[Product%20Item]"
     driver.get(url)
     time.sleep(3)
@@ -36,6 +39,12 @@ for page in range(0, 1000, 10):
         item.append(f'https://schroff.nvent.com/ru-ru/products/enc{item_id}/pdf')
         items.append(item)
 
+# Создаем описание магазина
+root = etree.Element('catalog', date=str(datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S%z")) + "+03:00")
+shop = etree.SubElement(root, 'shop')
+etree.SubElement(shop, 'name').text = "Schroff"
+etree.SubElement(shop, 'company').text = 'nVent'
+
 for item in items:
     driver.get(item[1])
     time.sleep(3)
@@ -44,6 +53,7 @@ for item in items:
     name = driver.find_element(By.CLASS_NAME, 'product-hero__title').text
     description = driver.find_element(By.CLASS_NAME, 'product-hero__description').text
     attributes = driver.find_element(By.CLASS_NAME, 'products-attribute__list').text
+
     # Артикул
     print(item_id)
     # Название позиции
@@ -53,17 +63,26 @@ for item in items:
     # Атрибуты позиции
     print(attributes)
 
+    # Создаем карточку товара
+    offer = etree.SubElement(shop, 'offer', id=str(item_id))
+    etree.SubElement(offer, 'name').text = name
+    etree.SubElement(offer, 'description').text = description
+    etree.SubElement(offer, 'attributes').text = attributes
+
     # Скачиваем картинки
     driver.find_element(By.XPATH, '//*[@id="block-mainpagecontent"]/div/div/div/div[2]/div[1]'
                                   '/div/div[2]/div/div[2]/ul').click()
     slick_track = driver.find_element(By.CLASS_NAME, "slick-track")
     images = slick_track.find_elements(By.TAG_NAME, "img")
+
     for image in images:
         image_url = image.get_attribute("src")  # Получение ссылки на картинку
         response = requests.get(image_url)
         response.raise_for_status()
+
         with open(f'images/{item_id}_{images.index(image)}.png.webp', 'wb') as file:
             file.write(response.content)
+        etree.SubElement(offer, f'picture').text = f"{item_id}_{images.index(image)}.png.webp"
 
     # Скачиваем документы
     driver.get(item[2])
@@ -71,15 +90,21 @@ for item in items:
     time.sleep(3)
 
     file_name = ""
+
     while file_name.lower().endswith('.pdf') is False:
         time.sleep(.25)
         try:
             file_name = max([download_dir + '/' + f for f in os.listdir(download_dir)], key=os.path.getctime)
             file_name = file_name.split("/")[-1]
-            # Название файла PDF
+            # Директория PDF
             print(file_name)
+            etree.SubElement(offer, 'document').text = file_name
         except ValueError:
             pass
+
+
+tree = etree.ElementTree(root)
+tree.write("file.xml", encoding="utf-8", xml_declaration=True)
 
 # Закрытие браузера
 driver.quit()
